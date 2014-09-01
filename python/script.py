a015e75284e69cdff1cae03614366abfaa8aa882
David Bradway <david.bradway@gmail.com>
2014-09-01 14:26:40 -0400

    add sequence button, update script

--- python/script.py
+++ python/script.py
@@ -2,22 +2,14 @@ import webiopi
 
 GPIO = webiopi.GPIO
 
-DOOR_IS_OPEN = False
-
-Garage = 0 # GPIO pin using BCM numbering
+GARAGE = 0 # GPIO pin using BCM numbering
 
 # setup function is automatically called at WebIOPi startup
 def setup():
     # set the GPIO used by the light to output
-    GPIO.setFunction(Garage, GPIO.OUT)
+    GPIO.setFunction(GARAGE, GPIO.OUT)
 
 # loop function is repeatedly called by WebIOPi
 def loop():
     # gives CPU some time before looping again
     webiopi.sleep(1)
-
-#@webiopi.macro
-#def toggleDoorState():
-#    global DOOR_IS_OPEN
-#    DOOR_IS_OPEN = not DOOR_IS_OPEN
-#    return DOOR_IS_OPEN
